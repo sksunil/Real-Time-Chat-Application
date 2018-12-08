4baee74061354c1804d0c6f4ee3e10511fb6084d
sksunil <sksunilkumawat1995@gmail.com>
2018-12-08 16:24:44 +0530

    create message component

---
+++ resources/js/components/message.vue
@@ -0,0 +1,11 @@
+<template>
+      <li class="list-group-item">Hello...</li>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -13,11 +13,11 @@
        <div class="row" id="app">
               <ul class="list-group offset-4 col-4">
                 <li class="list-group-item active">Chat Room</li>
-                <li class="list-group-item">Hello...</li>
+                <message></message>
 
-                <li class="list-group-item">How are you.</li>
-        
-                 <input type="text" class="form-control" placeholder="Write your message...">
+                 <input type="text" v-model="message" @keyup.enter='send'
+                 class="form-control" placeholder="Write your message...">
+
               </ul>
        </div>
    </div>
